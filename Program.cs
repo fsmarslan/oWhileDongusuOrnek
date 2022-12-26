10ff9fccfffb04d3ba73ca5f3008395de6ed214e
Fatih Arslan <fatih1122335@gmail.com>
2022-12-26 20:12:43 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oWhileDongusuOrnek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("lütfen faktoriyeli hesaplanacak sayıyı giriniz.");
+            int sayi = int.Parse(Console.ReadLine());
+            int faktoriyel = 1;
+
+            while (sayi>1)
+            {
+                faktoriyel *= sayi;
+                sayi--;
+            }
+            Console.WriteLine("foktoriyel: {0}",faktoriyel);
+            Console.ReadKey();
+        }
+    }
+}
